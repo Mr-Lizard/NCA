ea6bf4b2c5f69496c6bd6aa975c8bb5586473e30
KLEstes <klestes@gmail.com>
2018-03-30 09:49:57 -0500

    messaging added

--- server/server.js
+++ server/server.js
@@ -1,17 +1,44 @@
 const path = require('path');
 const port = process.env.PORT || 3000;
+const http = require('http');
+
 const express = require('Express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-var app = express();
-
-app.use(express.static(publicPath));
-
 //console.log(__dirname + '/../public');
 console.log("- public path: " + publicPath);
 
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
-app.listen(3000, () => console.log(`Listening on port ${port}`));
+app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+   console.log ('S - new user connected!');
+
+   socket.on('createMessage', (message) => {
+       console.log('S - createMessage rxed', message);
+
+       io.emit('newMessage', {
+             from:  message.from,
+             text:  message.text,
+             createdAt:  new Date().getTime()
+       });
+   });
+
+//    socket.on('newEmail', (newEmail) => {
+//       console.log('S - newEmail', newEmail);
+//   });
+
+  socket.on('disconnect', () => {
+      console.log("S - disconnected from server");
+      });
+
+});
+
+server.listen(port, () => console.log(`START:  Listening on port ${port}`));
 
 
-/// listen on prot 3000
+//  EOF.
